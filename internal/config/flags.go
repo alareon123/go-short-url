f80236ce955fc52992f0afc1fa735214f3d49ff7
alareon123 <85997200+alareon123@users.noreply.github.com>
2023-07-03 22:07:28 +0300

    first commit

--- internal/config/flags.go
+++ internal/config/flags.go
@@ -7,6 +7,7 @@ var BaseAddressURL string
 
 func Init() {
 	flag.StringVar(&AppServerURL, "a", ":8080", "address and port to run server")
-	flag.StringVar(&BaseAddressURL, "b", "http://localhost:8080/", "base address for short urls")
+	flag.StringVar(&BaseAddressURL, "b", "http://localhost:8080", "base address for short urls")
 	flag.Parse()
+	BaseAddressURL += "/"
 }
